e0143065abe81c24de46c0ce0d6a949b09288c71
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-21 00:22:53 +0800

    feat: implement advice fetch; update description

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,15 +3,27 @@ import Quote from '@/components/reusable/Quote.vue';
 import Card from '@/components/reusable/Card.vue';
 import ButtonList from '@/components/reusable/ButtonList.vue';
 import Button from '@/components/reusable/Button.vue';
+import axios from "@/config/axios"
+import { onMounted, ref } from 'vue';
 
-// https://api.adviceslip.com/advice
+const advice = ref('');
+
+const fetchAdvice = async () => {
+    const { data } = await axios.get('/advice')
+    advice.value = data.slip.advice;
+}
+
+onMounted(async () => {
+    await fetchAdvice();
+})
+
 </script>
 
 <template>
     <div class="h-screen flex flex-col gap-5 justify-center items-center">
         <h1 class="font-bold text-indigo-600 italic text-4xl">AdviceVault</h1>
         <div class="w-96 text-center text-slate-500">
-            <p>Advice Vault is your go-to source for instant, uplifting advice designed to spark motivation and clarity in everyday situations. </p>
+            <p>Advice Vault a simple platform that generates random thoughtful advice to empower your choices and enhance your life."</p>
         </div>
         <div class="w-0 md:w-96 text-center mr-5 md:mr-0">
             <div class="flex items-center gap-2">
@@ -24,7 +36,7 @@ import Button from '@/components/reusable/Button.vue';
             </div>
         </div>
         <Card>
-            <Quote quote="Pag may tinanim, may aanihin" />
+            <Quote :quote="advice" />
         </Card> 
         <ButtonList>
                 <Button title="facebook" icon="pi pi-facebook" />
