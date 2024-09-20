e6eda5211bcf314525a3759cf803decef40ec5eb
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-21 00:27:57 +0800

    feat: add conditional render for quote

--- src/components/reusable/Quote.vue
+++ src/components/reusable/Quote.vue
@@ -12,12 +12,14 @@ defineProps({
             <button><i class="pi pi-clone text-indigo-600"></i></button>
         </div>
         <div class="text-center font-normal italic mb-5 text-indigo-700">
-            <div class="text-5xl flex justify-start ml-5">“</div> 
-            <p class="font-bold">
-                {{ quote }} 
-            </p>
-            <div class="text-5xl flex justify-end mr-5">
-                ”
+            <div v-if="quote">
+                <div class="text-5xl flex justify-start ml-5">“</div> 
+                <p class="font-bold">
+                    {{ quote }} 
+                </p>
+                <div class="text-5xl flex justify-end mr-5">
+                    ”
+                </div>
             </div>
         </div> 
     </div>
