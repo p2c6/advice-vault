471345262f6056bce3f1c39ff954595a2485fcf2
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-20 23:54:56 +0800

    chore: add apostrophe

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,9 +1,24 @@
 <script setup>
 
+defineProps({
+    quote: String,
+    require: true
+});
 </script>
 
 <template>
     <div>
-        <p class="text-center">Pag may tinanim may annihin</p>
+        <div class="flex justify-end">
+            <button><i class="pi pi-clone text-indigo-600"></i></button>
+        </div>
+        <div class="text-center font-normal italic mb-5 text-indigo-700">
+            <div class="text-5xl flex justify-start ml-5">“</div> 
+            <p class="font-bold">
+                {{ quote }} 
+            </p>
+            <div class="text-5xl flex justify-end mr-5">
+                ”
+            </div>
+        </div> 
     </div>
 </template>
