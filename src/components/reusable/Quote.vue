4aa367fbe6746fd6309f091fbeb6c558caf5f2e4
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-21 16:16:31 +0800

    style: adjust text responsiveness

--- src/components/reusable/Quote.vue
+++ src/components/reusable/Quote.vue
@@ -31,11 +31,11 @@ const handleCopyClick = () => {
         </div>
         <div class="text-center font-normal italic mb-5 text-indigo-700">
             <div v-if="!isLoading && quote">
-                <div class="text-5xl flex justify-start ml-5">“</div> 
-                <p class="font-bold" id="quote">
+                <div class="text-3xl md:text-5xl flex justify-start ml-5">“</div> 
+                <p class="text-sm md:text-md lg:text-md  font-bold" id="quote">
                     {{ quote }} 
                 </p>
-                <div class="text-5xl flex justify-end mr-5">
+                <div class="text-3xl md:text-5xl flex justify-end mr-5">
                     ”
                 </div>
             </div>
